4dc8d935948214f62ec5c9f97ccdcc31d4e6a824
gesemaya <pelotoken@gmail.com>
2023-11-05 10:20:44 +0800

    first commit

---
+++ src/exchanges/bybit/bybit.api.ts
@@ -0,0 +1,68 @@
+import axios, { AxiosHeaders } from 'axios';
+import retry, { isNetworkError } from 'axios-retry';
+import createHmac from 'create-hmac';
+import qs from 'qs';
+
+import type { ExchangeOptions } from '../../types';
+import { virtualClock } from '../../utils/virtual-clock';
+
+import { BASE_URL, BROKER_ID, ENDPOINTS, RECV_WINDOW } from './bybit.types';
+
+const isErrorSignature = (error: any) => {
+  const isNetwork = isNetworkError(error);
+  const isErrSign =
+    error?.response?.data?.ret_msg?.includes('error sign!') ||
+    error?.response?.data?.retMsg?.includes('error sign!');
+  return isNetwork || isErrSign;
+};
+
+export const createAPI = (options: ExchangeOptions) => {
+  const xhr = axios.create({
+    baseURL: BASE_URL[options.testnet ? 'testnet' : 'livenet'],
+    timeout: options?.extra?.recvWindow ?? RECV_WINDOW,
+    paramsSerializer: {
+      serialize: (params) => qs.stringify(params),
+    },
+    headers: {
+      'X-Referer': BROKER_ID,
+      'X-BAPI-RECV-WINDOW': RECV_WINDOW,
+      'Content-Type': 'application/json, charset=utf-8',
+    },
+  });
+
+  // retry requests on network errors instead of throwing
+  // and some signature errors
+  retry(xhr, {
+    retries: 3,
+    retryCondition: isErrorSignature,
+  });
+
+  xhr.interceptors.request.use((config) => {
+    const nextConfig = { ...config };
+
+    if (config.method === 'get' && config.url === ENDPOINTS.KLINE) {
+      return { ...nextConfig, headers: new AxiosHeaders({}) };
+    }
+
+    const data =
+      config.method === 'get'
+        ? qs.stringify(config.params)
+        : JSON.stringify(config.data);
+
+    const timestamp = virtualClock.getCurrentTime().valueOf();
+    const signature = createHmac('sha256', options.secret)
+      .update([timestamp, options.key, RECV_WINDOW, data].join(''))
+      .digest('hex');
+
+    const headers = new AxiosHeaders({
+      ...nextConfig.headers,
+      'X-BAPI-SIGN': signature,
+      'X-BAPI-API-KEY': options.key,
+      'X-BAPI-TIMESTAMP': timestamp,
+    });
+
+    return { ...nextConfig, headers };
+  });
+
+  return xhr;
+};
---
+++ src/exchanges/okx/okx.api.ts
@@ -0,0 +1,63 @@
+import axios, { AxiosHeaders } from 'axios';
+import retry, { isNetworkError } from 'axios-retry';
+import createHmac from 'create-hmac';
+import qs from 'qs';
+
+import type { ExchangeOptions } from '../../types';
+import { virtualClock } from '../../utils/virtual-clock';
+
+import { BASE_URL, RECV_WINDOW } from './okx.types';
+
+export const createAPI = (options: ExchangeOptions) => {
+  const { passphrase } = options;
+
+  if (!passphrase) {
+    throw new Error('OKX requires a passphrase');
+  }
+
+  const baseURL = options.corsAnywhere
+    ? `${options.corsAnywhere}/${BASE_URL}`
+    : BASE_URL;
+
+  const xhr = axios.create({
+    baseURL,
+    timeout: options?.extra?.recvWindow ?? RECV_WINDOW,
+    headers: options.testnet ? { 'x-simulated-trading': 1 } : {},
+  });
+
+  retry(xhr, { retries: 3, retryCondition: isNetworkError });
+
+  xhr.interceptors.request.use((config) => {
+    if (config?.url?.includes('public')) {
+      return config;
+    }
+
+    const nextConfig = { ...config };
+
+    const method = config.method?.toUpperCase?.() || 'GET';
+    const params = config.params
+      ? decodeURIComponent(qs.stringify(config.params))
+      : null;
+    const url = `${config.url}${params ? `?${params}` : ''}`;
+    const data = config.data ? JSON.stringify(config.data) : null;
+
+    const timestamp = virtualClock.getCurrentTime().toISOString();
+    const toSign = [timestamp, method, url, data ? data : ''];
+
+    const signature = createHmac('sha256', options.secret)
+      .update(toSign.join(''))
+      .digest('base64');
+
+    const headers = new AxiosHeaders({
+      ...nextConfig.headers,
+      'OK-ACCESS-KEY': options.key,
+      'OK-ACCESS-SIGN': signature,
+      'OK-ACCESS-TIMESTAMP': timestamp,
+      'OK-ACCESS-PASSPHRASE': passphrase,
+    });
+
+    return { ...nextConfig, headers };
+  });
+
+  return xhr;
+};
